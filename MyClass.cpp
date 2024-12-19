f186c9ab7ed85d2f85cdeb6f40dfccc1a39b4c18
tsekott <zekotmaryana2004@gmail.com>
2024-12-19 02:07:52 +0200

    Updated FuncA implementation in branchA

--- MyClass.cpp
+++ MyClass.cpp
@@ -1,6 +1,15 @@
 #include "MyClass.h"
 #include <cmath>
-
-double MyClass::FuncA() {
-    return 0.0; // Initial implementation returns 0
+double MyClass::FuncA(int n) {
+    /**
+     * Calculates the sum of the first n elements of the infinite sequence.
+     * @param n The number of terms to calculate in the series.
+     * @return The sum of the first n elements.
+     */
+    double result = 0.0;
+    for (int i = 1; i <= n; ++i) {
+        // Example calculation using the given sequence formula.
+        result += std::pow(-1, i) * std::pow(x, 2*i-1) / (2*i - 1);
+    }
+    return result;
 }
