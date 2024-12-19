b1fa97f3d20d72321fedc1f00e5a40c0084efb48
tsekott <zekotmaryana2004@gmail.com>
2024-12-19 02:34:17 +0200

    Add default parameter to FuncA and update build dependencies

--- MyClass.cpp
+++ MyClass.cpp
@@ -1,9 +1,10 @@
+// MyClass.cpp
 #include "MyClass.h"
 #include <cmath>
 
 double MyClass::FuncA(int n) {
     double result = 0.0;
-    double x = 1.0; // Example value for x, you can change it as needed
+    double x = 1.0;  // Example value for x
     for (int i = 1; i <= n; ++i) {
         result += std::pow(-1, i) * std::pow(x, 2*i-1) / (2*i - 1);
     }
--- MyClass.h
+++ MyClass.h
@@ -1,10 +1,11 @@
+// MyClass.h
 #ifndef MYCLASS_H
 #define MYCLASS_H
 
 class MyClass {
 public:
     // FuncA calculates a trigonometric function
-    double FuncA(int n);
+    double FuncA(int n = 10); // Default parameter
 };
 
 #endif
